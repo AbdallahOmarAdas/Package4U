135bd5ccf10e8cf6409b4097174dd81150b47c57
Abdallah Adas <AAdas@asaltech.com>
2023-12-17 02:00:25 +0200

    delete content_page_driver.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/customer/add_parcel.dart';
-import 'package:flutter_application_1/customer/call.dart';
-import 'package:flutter_application_1/customer/edit_profile.dart';
 import 'package:flutter_application_1/customer/from_me.dart';
-import 'package:flutter_application_1/customer/home.dart';
 import 'package:flutter_application_1/customer/main_page.dart';
-import 'package:flutter_application_1/customer/set_location.dart';
-import 'package:flutter_application_1/drivers/content_page_driver.dart';
 import 'package:flutter_application_1/drivers/home_page_driver.dart';
 import 'package:flutter_application_1/drivers/onGoing.dart';
 import 'package:flutter_application_1/employee/main_page.dart';
-import 'package:flutter_application_1/manager/creat_driver.dart';
-import 'package:flutter_application_1/manager/creat_employee.dart';
 import 'package:flutter_application_1/manager/main_page.dart';
-import 'package:flutter_application_1/sign_in_up_pages/regstration.dart';
 import 'package:flutter_application_1/sign_in_up_pages/sign_in.dart';
-import 'package:flutter_application_1/sign_in_up_pages/testAPI.dart';
 import 'package:flutter_application_1/style/common/theme_h.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'package:get_storage/get_storage.dart';
 
 void main() async {
